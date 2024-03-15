2bf0245f544e864a15097b19c35f1199f506224c
whitney shisia <shisiawhitney215@gmail.com>
2024-03-15 07:35:32 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const writeCards = (names, occasion) => {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return messages;
+  };
+  
+  const names = ["whit", "jane", "whishi"];
+  const occasion = "birthday";
+  const messages = writeCards(names, occasion);
+  console.log(messages);
+
+  const countDown = (number) => {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+};
+countDown(5); 
